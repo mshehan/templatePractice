44b664175dddfc7af2c813957ee8e3585608be96
Matthew Shehan <mshehan@ucsc.edu>
2016-11-08 17:37:56 -0800

    practice with mustache.js templating

---
+++ js/department.js
@@ -0,0 +1,22 @@
+/*
+/Matthew Shehan
+//mshehan@my.smccd.edu
+//CIS 
+//114 OL
+//department.js
+//assignment 7
+// 05-13-2016
+ */
+$(document).ready(function () {
+    $.getJSON("assign7files/department.json", function (data) {
+        var dept = "{{#depts}}<h2>{{name}}</h2>"
+            + "<p>{{#employees}}{{>employee}}{{/employees}}</p>"
+            + "{{/depts}}";
+        var emp_partial = {
+            employee: "<img src='{{pic}}' />"
+            + "{{firstName}} {{lastName}}, {{title}}<br>"
+        };
+        var output = Mustache.to_html(dept, data, emp_partial);
+        $('#employees').html(output);
+    });
+});
